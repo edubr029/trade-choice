4b519c88f296aacdd9040d728a422c5322eac7a6
Eduardo Fonseca <me@eduu.xyz>
2026-07-11 14:31:58 -0300

    feat: scaffold trade-choice mod project for MC 26.2
    - Fabric Loader 0.19.3, Fabric API 0.154.2+26.2, Loom 1.17
    - Java 25, Gradle 9.5.1, unobfuscated Mojang mappings
    - Split client/main source sets with mixin configs
    - Soft dependency on trade_cycling
    - Build verified passing

---
+++ src/client/java/com/tradechoice/client/TradeChoiceClient.java
@@ -0,0 +1,9 @@
+package com.tradechoice.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradeChoiceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
